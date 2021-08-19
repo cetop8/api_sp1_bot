7b485644a28703cb18095d44b424451a23bb3d00
Алексей Файферт <cetop8@yandex.ru>
2021-08-19 22:10:21 +0300

    v1.2

--- homework.py
+++ homework.py
@@ -24,36 +24,34 @@ handler.setFormatter(logging.Formatter('%(levelname)s, %(message)s'))
 logger.addHandler(handler)
 
 try:
-    PRAKTIKUM_TOKEN = os.environ['PRAKTIKUM_TOKEN']
-    TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
-    CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+    PRAKTIKUM_TOKEN = os.environ.get('PRAKTIKUM_TOKEN')
+    TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+    CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 except KeyError:
     logger.error('Ошибка в переменных токенов')
 
-bot_client = Bot(token=TELEGRAM_TOKEN)
-headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+BOT_CLIENT = Bot(token=TELEGRAM_TOKEN)
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+STATUSES = {
+    'reviewing': 'Проект на ревью.',
+    'rejected': 'К сожалению, в работе нашлись ошибки.',
+    'approved': 'Ревьюеру всё понравилось, работа зачтена!',
+}
 time.sleep(300)
 
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
-    verdict = 'unknown_status'
+    if homework_status not in STATUSES:
+        logger.error('Ошибка, неизвестный статус')
+        raise Exception('Ошибка, неизвестный статус')
     if homework_name is None or homework_status is None:
         logger.error('homework_name is None OR homework_status is None')
-        return (f'{homework_name}: '
-                f'homework_name is None OR homework_status is None')
-    if verdict == 'unknown_status':
-        logger.error('Ошибка, неизвестный статус')
-    statuses = {
-        'reviewing': 'Проект на ревью.',
-        'rejected': 'К сожалению, в работе нашлись ошибки.',
-        'approved': 'Ревьюеру всё понравилось, работа зачтена!',
-        'unknown_status': 'Ошибка, неизвестный статус'
-    }
-    return f'У вас проверили работу "{homework_name}"!' \
-        f'{statuses[homework_status]}'
+        raise Exception('homework_name is None OR homework_status is None')
+    return (f'У вас проверили работу "{homework_name} "!'
+            f'{STATUSES[homework_status]}')
 
 
 def get_homeworks(current_timestamp):
@@ -64,31 +62,31 @@ def get_homeworks(current_timestamp):
     try:
         homework_statuses = requests.get(
             url=URL,
-            headers=headers,
+            headers=HEADERS,
             params=params
         )
     except requests.exceptions.RequestException:
         logger.error('Request exception occurred', exc_info=True)
-        send_message('Request exception occurred', bot_client)
+        send_message('Request exception occurred', BOT_CLIENT)
         raise('Ошибка в реквесте при запросе к апи практикума')
 
     try:
         YP_request = homework_statuses.json()
     except json.decoder.JSONDecodeError:
         logger.error('JSONDecodeError occurred', exc_info=True)
-        send_message('JSONDecodeError occurred', bot_client)
+        send_message('JSONDecodeError occurred', BOT_CLIENT)
         raise('Ошибка при расшифровке json-файла')
     if 'error' in YP_request:
         logger.error(YP_request['error'])
-        send_message(YP_request['error'], bot_client)
+        send_message(YP_request['error'], BOT_CLIENT)
     return YP_request
 
 
 def send_message(message):
     try:
-        return bot_client.send_message(chat_id=CHAT_ID, text=message)
+        return BOT_CLIENT.send_message(chat_id=CHAT_ID, text=message)
     except Bot.ResponseError:
-        raise('Ошибка на стороне Telegram')
+        raise SystemExit('Ошибка на стороне телеграма')
 
 
 def main():
@@ -100,18 +98,18 @@ def main():
             new_homework = get_homeworks(current_timestamp)
             if new_homework.get('homeworks'):
                 last_hw = new_homework('homeworks')[0]
-                send_message((parse_homework_status(last_hw)), bot_client)
+                send_message((parse_homework_status(last_hw)), BOT_CLIENT)
                 logger.info('Message was sent')
             current_timestamp = new_homework('current_date',
                                              current_timestamp)
 
         except requests.exceptions.RequestException:
             logger.error('Exception occurred', exc_info=True)
-            send_message('Exception occurred', bot_client)
+            send_message('Exception occurred', BOT_CLIENT)
             time.sleep(5)
         except AttributeError:
             logger.error('AttributeError', exc_info=True)
-            send_message('AttributeError occurred', bot_client)
+            send_message('AttributeError occurred', BOT_CLIENT)
             time.sleep(5)
 
 
